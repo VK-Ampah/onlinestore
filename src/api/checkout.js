8c0741269545161861379bfdc9fa8cf3abad5613
Lorkhann <liam.philpott@keyin.com>
2023-12-23 12:51:14 -0330

    JSX Bugged

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -1,12 +1,3 @@
 fetch('https://fakestoreapi.com/checkout')
             .then(res=>res.json())
             .then(json=>console.log(json))
-
-    const Checkoutform = () => {
-        return (
-        <div className="page">
-            <checkoutform/>
-            Hey this is a test
-        </div>
-        );
-    }
