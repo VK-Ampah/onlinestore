b9cc15713304f1097859f90f0d1329ba36541b62
Lorkhann <liam.philpott@keyin.com>
2023-12-23 22:41:30 -0330

    Checkout

--- src/api/checkout.js
+++ src/api/checkout.js
@@ -1,3 +1,4 @@
 fetch('https://fakestoreapi.com/checkout')
             .then(res=>res.json())
             .then(json=>console.log(json))
+            const element = <h1 style={{ color: 'white' }}>Hello world</h1>
